96f69386eba48a35cb7a44e244d76e4d727c066b
Mike_Zhang <zhangwengyu999@gmail.com>
2021-10-30 14:30:48 +0800

    create some functions
    listAll(); listEnrolledWs(); findByXX() * 6;

---
+++ findByDateTime.py
@@ -0,0 +1,22 @@
+def findByDateTime(inDT):
+    file = open('workshop.txt',mode='r')
+    readLine = file.readlines()
+    outList = []
+    
+    for i in readLine:
+        lineList = i.strip("\n").split(":")
+        lineList[1] = lineList[1].split(" ")
+        outList.append(lineList)
+    outDict = dict(outList)
+    file.close()
+    
+    flag = False
+    print("--------Found:--------\n----------------------")
+    for d in outDict:
+        if (outDict[d][2] == inDT):
+            flag = True
+            print("workshop ID: "+d+"\nTitle:       "+outDict[d][0]+"\nLocation:    "+outDict[d][1]+"\nDate & Time: "+outDict[d][2]+"\nQuotas:      "+outDict[d][3]+"\nRemaining:   "+outDict[d][4]+"\n----------------------")
+    if (flag == False):
+        print("NOT FOUND!")
+
+findByDateTime('1200-1400')
---
+++ findBywID.py
@@ -0,0 +1,19 @@
+def findBywID(inwID):
+    file = open('workshop.txt',mode='r')
+    readLine = file.readlines()
+    outList = []
+    
+    for i in readLine:
+        lineList = i.strip("\n").split(":")
+        lineList[1] = lineList[1].split(" ")
+        outList.append(lineList)
+    outDict = dict(outList)
+    file.close()
+    
+    if (inwID not in outDict):
+        print("NO workshop with ID: "+inwID)
+    else:
+        print("--------Found:--------s\n----------------------")
+        print("workshop ID: "+inwID+"\nTitle:       "+outDict[inwID][0]+"\nLocation:    "+outDict[inwID][1]+"\nDate & Time: "+outDict[inwID][2]+"\nQuotas:      "+outDict[inwID][3]+"\nRemaining:   "+outDict[inwID][4]+"\n----------------------")
+
+findBywID("W000002")
